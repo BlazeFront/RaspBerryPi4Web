2132c6a386ac80956783deba6e8b7166e6563069
BlazeFront <Joel.Hammer007@yahoo.de>
2025-01-28 16:27:05 +0100

    initial

--- update_webapp.sh
+++ update_webapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the webapp directory
-cd /var/www/html/
+cd /var/www/html/RaspBerryPi4Web/
 
 # Pull the latest changes
 git reset --hard  # Optional: ensures no local changes conflict
@@ -9,6 +9,6 @@ git pull origin main  # Replace 'main' with your default branch if different
 
 # Set proper permissions (if needed)
 sudo chown -R www-data:www-data /var/www/html/
-sudo chmod -R 755 /var/www/html/
+sudo chmod -R 777 /var/www/html/
 
 echo "Webapp updated successfully."
