80235d575db8a9f6e424bf7e594f14b4c12748fd
BlazeFront <Joel.Hammer007@yahoo.de>
2025-01-30 10:04:13 +0100

    repeat

--- index.php
+++ index.php
@@ -164,7 +164,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
             cursor: pointer;
         }
 
-        .failed {
+        .error {
             color: rgb(167, 30, 30);
         }
 
