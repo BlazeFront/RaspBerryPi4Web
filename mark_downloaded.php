458e5c5940c06612b39e2d3ea83f539e0d5d0d3f
BlazeFront <Joel.Hammer007@yahoo.de>
2025-01-30 10:57:13 +0100

    repeat

--- mark_downloaded.php
+++ mark_downloaded.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
 // Get the video ID from the query string
 $id = isset($_GET['id']) ? (int)$_GET['id'] : null;
 $videoTitle = isset($_GET['title']) ? (int)$_GET['title'] : null;
-out.print($videoTitle);
+echo "<script>console.log('$videoTitle');</script>";
 
 if (!$id) {
     error_log("Invalid ID parameter provided.");
