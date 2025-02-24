7e71ef0a602c9fdfe559db22b389f8fb7212ec52
BlazeFront <Joel.Hammer007@yahoo.de>
2025-02-24 19:20:47 +0100

    repeat

--- index.php
+++ index.php
@@ -340,14 +340,14 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 
             rows.forEach(row => {
                 var newRow = document.createElement("tr");
-                if (row.downloaded == 1) {
+                if (row.downloaded == "1") {
                     newRow.innerHTML = `
                         <td class='remove-field' onclick='removeFromDatabase("${row.id}", this)'><i class='fa-solid fa-angles-left'></i></td>
                         <td class='url-cell' onclick='copyToClipboard("${row.name}")'>${row.name}</td>
                         <td class='icon-cell' style='cursor:pointer' onclick='copyToClipboard("${row.url}")'><i class='fa-regular fa-copy'></i></td>
                         <td><i style='cursor:pointer' class='fa-solid fa-square-check' onclick='toggleDownloaded(${row.id}, this)'></i></td>
                         <td class='icon-cell'><a href='${row.url}' target='_blank'><i class='fab fa-youtube'></i></a></td>
-                        <td value='${row.id}' class='download-icon'><i class='fas fa-check' style='color: green;'></i></td>
+                        <td value='${row.id}' class='download-icon'><a href='javascript:void(0);' onclick='markDownloaded(${row.id}, "${row.name}", this)'><i class='fas fa-download'></i></a></td>
                     `;
                 } else {
                     newRow.innerHTML = `
@@ -356,9 +356,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
                         <td class='icon-cell' style='cursor:pointer' onclick='copyToClipboard("${row.url}")'><i class='fa-regular fa-copy'></i></td>
                         <td><i class='fa-regular fa-square' onclick='toggleDownloaded(${row.id}, this)'></i></td>
                         <td class='icon-cell'><a href='${row.url}' target='_blank'><i class='fab fa-youtube'></i></a></td>
-                        <td value='${row.id}' class='download-icon'>
-                            <a href='javascript:void(0);' onclick='markDownloaded(${row.id}, "${row.name}", this)'><i class='fas fa-download'></i></a>
-                        </td>
+                        <td value='${row.id}' class='download-icon'><a href='javascript:void(0);' onclick='markDownloaded(${row.id}, "${row.name}", this)'><i class='fas fa-download'></i></a></td>
                     `;
                 }
                 tbody.appendChild(newRow);
