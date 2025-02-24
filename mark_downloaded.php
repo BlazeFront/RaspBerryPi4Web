c472bd2f36a59ca9149d84ea673e3a44321ff5df
BlazeFront <Joel.Hammer007@yahoo.de>
2025-02-24 20:40:30 +0100

    repeat

--- mark_downloaded.php
+++ mark_downloaded.php
@@ -75,7 +75,7 @@ $safeTitle = preg_replace('/[\/:*?"<>|]/', '', $videoTitle);
 $outputFile = __DIR__ . "/downloads/" . $safeTitle . ".mp3";
 
 // Check if the file already exists
-if (file_exists($outputFile)) {
+/*if (file_exists($outputFile)) {
     // Serve the MP3 file immediately
     header('Content-Type: audio/mpeg');
     header('Content-Disposition: attachment; filename="' . basename($outputFile) . '"');
@@ -95,7 +95,7 @@ if (file_exists($outputFile)) {
     }
     
     exit();
-}
+}*/
 
 echo("URL: " . escapeshellarg($cleanUrl));
 echo("TITLE: " . escapeshellarg($outputFile));
