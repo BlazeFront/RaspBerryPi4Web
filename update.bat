e43b88e591d00b9fcf70351c1d0a927416cd35be
BlazeFront <Joel.Hammer007@yahoo.de>
2025-03-02 23:20:16 +0100

    repeat

--- update.bat
+++ update.bat
@@ -1,14 +1,36 @@
 @echo off
-:: Stage all changes
-git add .
+:loop
+cls
+
+echo Make a new release:
 
 :: Prompt the user for a commit message
-set /p commitMessage=Enter commit message: 
+set /p commitMessage=commit: 
 
-:: Commit with the provided message
-git commit -m "%commitMessage%"
+:: Check if the user wants to exit
+if /I "%commitMessage%"=="exit" goto end
 
+:: Declare a variable for the commit message
+set message=%commitMessage%
+
+echo.
+echo.
+git add .
+
+:: Commit with the variable
+git commit -m "%message%"
+
+echo.
 :: Push to the repository
 git push origin main
 
+echo.
+echo =====================================
+echo.
+echo Commit and push completed.
+echo.
 pause
+goto loop
+
+:end
+echo Exiting...
